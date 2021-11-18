e2759516d087bd1f0646a91a08032bdc959da535
sangjun.yoon <sangjun.yoon@gmail.com>
2021-11-18 15:12:04 +0900

    refactory : 별 찍기 리팩토링

--- Quest03/skeleton/drawStar.js
+++ Quest03/skeleton/drawStar.js
@@ -1,21 +1,41 @@
-function star(){
-  const nString = prompt('별모양의 높이를 알려주세요.')
-  const n = parseInt(nString);
-  const array = [];
+function repeat({initial = 0,final,increaseRate = 1,f}){
+  for(let i = initial; i < final; i += increaseRate){
+    f();
+  }
+}
+
+function drawTree(array,n){
   for(let i = 0; i < n; i++){
     let line = '';
-    for(let k = 0; k < n-i+1; k++){
-      line += ' ';
-    }
-    for(let k = 0; k < 2*i+1; k++){
-      line += '*';
-    }
-    for(let k = 0; k < n-i+1; k++){
-      line += ' ';
-    }
+    repeat({
+      final : n+1-i,
+      f : () => line += " "
+    })
+    repeat({
+      final : 2*i+1,
+      f : () => line += "*"
+    })
     array.push(line);
   }
-  const result = array.join('\n');
+  return array.join('\n');
+}
+
+function star(){
+  const nString = prompt('삼각형의 높이를 알려주세요.');
+  if(!(nString - 1) || nString.length === 0){
+    const askAgain = confirm('공백또는 문자를 입력하면 안됩니다. 다시 입력하시겠습니까?')
+    if(askAgain){
+      return star();
+    }
+    return;
+  }
+  console.log('삼각형을 생성합니다');
+  const n = parseInt(nString);
+  const array = [];
+  const result = drawTree(array,n);
   console.log(result);
 }
+
 star()
+
+

c066092e3a3d00524869abdac11c43bd2752b55c
sangjun.yoon <sangjun.yoon@gmail.com>
2021-11-23 10:38:40 +0900

    add : menubar 추가

--- Quest04/skeleton/desktop.js
+++ Quest04/skeleton/desktop.js
@@ -6,16 +6,14 @@ class Window {
     this.name = name;
     this.draggable = false;
     this.element = document.createElement("div");
-    this.mouseOffset = {
-      x: 0,
-      y: 0,
-    };
   }
   makeCloseButton() {
     const closeButton = document.createElement("button");
     closeButton.innerText = "x";
+    closeButton.classList.add("close-button");
     this.element.appendChild(closeButton);
-    closeButton.onclick = () => {
+    closeButton.onclick = (e) => {
+      e.stopPropagation();
       this.root.removeChild(this.element);
     };
   }
@@ -30,7 +28,7 @@ class Window {
             name: child.name,
           });
           folder.render(targetElement);
-          folder.doubleClick();
+          folder.addDoubleClickEvent();
           folder.element.classList.add("folder");
           break;
         case "icon":
@@ -45,38 +43,47 @@ class Window {
       }
     });
   }
-  render(targetElement) {
-    this.element.innerText = this.name;
+  render(targetElement, title) {
+    if (title) {
+      const titleElement = document.createElement("div");
+      titleElement.innerText = this.name;
+      titleElement.classList.add("title", "drag");
+      this.element.appendChild(titleElement);
+    } else {
+      this.element.innerText = this.name;
+      this.element.classList.add("drag");
+    }
     targetElement.appendChild(this.element);
     this.addDragEvent(this.element);
   }
 
   addDragEvent() {
     this.element.addEventListener("mousedown", (e) => {
+      if (!e.target.classList.contains("drag")) return;
       e.preventDefault();
       e.stopPropagation();
-      this.mouseOffset = {
-        x: e.target.offsetLeft - e.clientX,
-        y: e.target.offsetTop - e.clientY,
-      };
       this.draggable = true;
       this.element.style.zIndex = 2;
+      this.mouseOffset = {
+        x: this.element.offsetLeft - e.clientX,
+        y: this.element.offsetTop - e.clientY,
+      };
     });
     this.element.addEventListener("mousemove", (e) => {
-      e.preventDefault();
-      e.stopPropagation();
+      if (!e.target.classList.contains("drag")) return;
       if (!this.draggable) return;
-      e.target.style.position = "absolute";
       const newLocationX = e.clientX + this.mouseOffset.x + "px";
       const newLocationY = e.clientY + this.mouseOffset.y + "px";
-      e.target.style.left = newLocationX;
-      e.target.style.top = newLocationY;
+      this.element.style.left = newLocationX;
+      this.element.style.top = newLocationY;
+      e.target.style.position = "absolute";
     });
     this.element.addEventListener("mouseleave", () => {
       this.draggable = false;
       this.element.style.zIndex = 0;
     });
-    this.element.addEventListener("mouseup", () => {
+    this.element.addEventListener("mouseup", (e) => {
+      e.stopPropagation();
       this.draggable = false;
       this.element.style.zIndex = 0;
     });
@@ -88,37 +95,36 @@ class Desktop extends Window {
   }
 }
 class Icon extends Window {
-  constructor({ targetElement, children, name }) {
-    super({ targetElement, children, name });
+  constructor({ targetElement, name }) {
+    super({ targetElement, name });
   }
 }
 
 class Folder extends Window {
+  #isOpen = false;
+  #openedFolder = null;
   constructor({ root, targetElement, children, name }) {
     super({ root, targetElement, children, name });
-    this.isOpen = false;
-    this.openedFolder = null;
   }
-  doubleClick() {
+  addDoubleClickEvent() {
     this.element.addEventListener("dblclick", () => {
-      if (this.isOpen && this.openedFolder) {
-        this.root.appendChild(this.openedFolder);
+      if (this.#isOpen && this.#openedFolder) {
+        this.root.appendChild(this.#openedFolder);
         return;
       }
-      if (this.isOpen) return;
+      if (this.#isOpen) return;
       const newWindow = new Window({
         root: this.root,
         targetElement: this.targetElement,
         name: this.name,
         children: this.children,
       });
-      newWindow.render(this.root);
+      newWindow.render(this.root, true);
       newWindow.renderChild(newWindow.element);
       newWindow.element.classList.add("window");
       newWindow.makeCloseButton();
-      console.log(this.targetElement, newWindow.element);
       this.isOpen = true;
-      this.openedFolder = newWindow.element;
+      this.#openedFolder = newWindow.element;
     });
   }
 }

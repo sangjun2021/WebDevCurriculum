7065bc0c91f6f33bb78e033132a1d6615aa24588
sangjun.yoon <sangjun.yoon@gmail.com>
2021-11-29 09:54:21 +0900

    refactory : 중복코드 합성

--- Quest05/skeleton/components/Files.js
+++ Quest05/skeleton/components/Files.js
@@ -1,48 +1,19 @@
-import Storage from "../utils/Storage.js";
-import Event from "../utils/Event.js";
-class Files {
-  #state;
-  #targetElement;
-  #event = new Event();
-  #localStorage = new Storage(window.localStorage);
+import List from "./List.js";
+
+class File {
+  #list;
   constructor(targetElement) {
-    this.#targetElement = targetElement;
-    this.#targetElement.addEventListener("click", (e) => {
-      const id = e.target.closest("li").dataset.id;
-      if (e.target.id === "js-close-button") {
-        this.#event.dispatch("onDeleteFile", id);
-      } else {
-        this.#event.dispatch("onClickFile", id);
-      }
+    this.#list = new List({
+      targetElement,
+      className: "file",
+      deleteEvent: "onDeleteFile",
+      clickEvent: "onClickFile",
+      storage: window.localStorage,
     });
-  }
-  #render() {
-    try {
-      this.#targetElement.innerHTML = this.#state
-        .map((file) => {
-          return `<li class="file ${
-            file.isSelected ? "js-selected" : ""
-          }" data-id="${file.id}">
-          <span class="title ${file.isEdited ? "js-edited" : ""}">${
-            file.title
-          }</span>
-            <button id="js-close-button" class=>X</button>
-          </li>`;
-        })
-        .join("");
-    } catch (e) {
-      console.log(e.message);
-      return;
-    }
   }
   setState(id) {
-    const nextState = this.#localStorage.getList().map((tab) => {
-      if (tab.id !== id) return { ...tab, isSelected: false };
-      return { ...tab, isSelected: true };
-    });
-    this.#state = nextState;
-    this.#render();
+    this.#list.setState(id);
   }
 }
 
-export default Files;
+export default File;
--- Quest05/skeleton/components/Tab.js
+++ Quest05/skeleton/components/Tab.js
@@ -1,47 +1,18 @@
-import Storage from "../utils/Storage.js";
-import Event from "../utils/Event.js";
+import List from "./List.js";
+
 class Tab {
-  #state;
-  #targetElement;
-  #event = new Event();
-  #sessionStorage = new Storage(window.sessionStorage);
+  #list;
   constructor(targetElement) {
-    this.#targetElement = targetElement;
-    this.#targetElement.addEventListener("click", (e) => {
-      const id = e.target.closest("li").dataset.id;
-      if (e.target.id === "js-close-button") {
-        this.#event.dispatch("onDeleteTab", id);
-      } else {
-        this.#event.dispatch("onClickTab", id);
-      }
+    this.#list = new List({
+      targetElement,
+      className: "tab",
+      deleteEvent: "onDeleteTab",
+      clickEvent: "onClickTab",
+      storage: window.sessionStorage,
     });
-  }
-  #render() {
-    try {
-      this.#targetElement.innerHTML = this.#state
-        .map((file) => {
-          return `<li class="tab ${
-            file.isSelected ? "js-selected" : ""
-          }" data-id="${file.id}">
-          <span class="title ${file.isEdited ? "js-edited" : ""}">${
-            file.title
-          }</span>
-            <button id="js-close-button" class=>X</button>
-          </li>`;
-        })
-        .join("");
-    } catch (e) {
-      console.log(e.message);
-      return;
-    }
   }
   setState(id) {
-    const nextState = this.#sessionStorage.getList().map((tab) => {
-      if (tab.id !== id) return { ...tab, isSelected: false };
-      return { ...tab, isSelected: true };
-    });
-    this.#state = nextState;
-    this.#render();
+    this.#list.setState(id);
   }
 }
 

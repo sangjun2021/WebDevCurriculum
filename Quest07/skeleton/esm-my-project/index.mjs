a2278b188a529a8d2d6d771a248a2848766469fc
sangjun.yoon <sangjun.yoon@gmail.com>
2021-12-06 09:36:41 +0900

    add : 퀘스트7 추가

--- Quest07/skeleton/esm-my-project/index.mjs
+++ Quest07/skeleton/esm-my-project/index.mjs
@@ -1,1 +1,11 @@
 // TODO: cjs-package와 esm-package의 함수와 클래스들을 가져다 쓰고 활용하려면 어떻게 해야 할까요?
+import { EsmUtilClass, esmUtilFunction } from "esm-package";
+import { cjsUtilFunction, CjsUtilClass } from "cjs-package";
+
+console.log("esmUtilFunction(hello) : ", esmUtilFunction("hello esm"));
+const esmTest = new EsmUtilClass(421);
+console.log("esmTest.double() : ", esmTest.double());
+
+console.log("cjsUtilFunction(hello) : ", cjsUtilFunction("hello cjs"));
+const cjsTest = new CjsUtilClass(123);
+console.log("cjsTest.double() : ", cjsTest.double());
--- Quest07/skeleton/esm-package/index.mjs
+++ Quest07/skeleton/esm-package/index.mjs
@@ -1,15 +1,15 @@
-class EsmUtilClass {
-    constructor(foo) {
-        this.foo = foo;
-    }
+export class EsmUtilClass {
+  constructor(foo) {
+    this.foo = foo;
+  }
 
-    double() {
-        return foo * 2;
-    }
+  double() {
+    return this.foo * 2;
+  }
 }
 
-const esmUtilFunction = str => {
-    return str.toUpperCase();
+export const esmUtilFunction = (str) => {
+  return str.toUpperCase();
 };
 
 // TODO: 다른 패키지가 EsmUtilClass와 esmUtilFunction를 가져다 쓰려면 어떻게 해야 할까요?

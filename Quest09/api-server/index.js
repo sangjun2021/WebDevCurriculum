568db7eb467763db6b84d8511ce7db6256a1e387
sangjun.yoon <sangjun.yoon@gmail.com>
2021-12-10 20:25:26 +0900

    feat : CRUD api 구현

--- Quest09/api-server/index.js
+++ Quest09/api-server/index.js
@@ -1,28 +1,40 @@
 const express = require("express");
 const File = require("./lib/File");
+const fs = require("fs/promises");
+const { v4: uuidv4 } = require("uuid");
 const app = express();
-
+const FileHandler = new File();
 app.use(express.json());
-app.get("/post-list", (req, res) => {
-  File.getFileList(res);
+app.get("/post/list", async (req, res) => {
+  const data = await FileHandler.getFileList();
+  res.status(200).send(data);
 });
-app.get("/post/:id", (req, res) => {
-  const id = req.params.id;
-  File.getFile(res, id, () => {
-    res.send("ok");
-  });
+app.get("/post/:id", async (req, res) => {
+  const { id } = req.params;
+  const data = await FileHandler.getFile(id);
+  res.status(200).send(data);
 });
-app.post("/post/:id", (req, res) => {
-  const id = req.params.id;
-  File.createFile(res, id, req.body.text);
+app.post("/post/", async (req, res) => {
+  const id = uuidv4();
+  const { title, text } = req.body;
+  await FileHandler.writeFile(id, title, text);
+  res.status(200).send(id);
 });
-app.delete("/post/:id", (req, res) => {
-  console.log(req.body);
-  const id = req.params.id;
+app.delete("/post/:id", async (req, res) => {
+  const { id } = req.params;
+  await FileHandler.deleteFile(id);
+  res.status(200).send("ok");
 });
-app.put("/post/:id", (req, res) => {
-  console.log(req.body);
-  const id = req.params.id;
+app.put("/post/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, text } = req.body;
+  await FileHandler.writeFile(id, title, text);
+  res.status(200).send("ok");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 app.listen(8080, () => {
   console.log("server is running aot 8080");
--- Quest09/api-server/lib/File.js
+++ Quest09/api-server/lib/File.js
@@ -1,35 +1,41 @@
-const fs = require("fs");
-
-module.exports = {
-  getFile: (res, id, callback) => {
-    const readStream = fs.createReadStream(`files/${id}.json`);
-    readStream.on("open", () => {
-      readStream.pipe(res);
-    });
-    readStream.on("finish", () => {
-      callback();
-    });
-  },
-  deleteFile: (res, id) => {},
-  updateFile: (res, id, data) => {},
-  createFile: (res, id, text) => {
-    fs.writeFile(
+const fsPromise = require("fs/promises");
+class File {
+  async getFile(id) {
+    const fileHandler = await fsPromise.open(`files/${id}.json`);
+    return fileHandler.readFile({ encoding: "utf-8" });
+  }
+  async deleteFile(id) {
+    await fsPromise.unlink(`files/${id}.json`);
+    return this.#deleteFileList(id);
+  }
+  async writeFile(id, title, text) {
+    await fsPromise.writeFile(
       `files/${id}.json`,
-      JSON.stringify({ title: "새파일", text }),
-      (err) => {
-        if (err) throw err;
-        res.send("ok");
-      }
+      JSON.stringify({ title, text })
     );
-  },
-  getFileList: (res) => {
-    fs.readdir("files", (error, files) => {
-      let buffer = [];
-      files.forEach((file) => {
-        const id = file.split(".")[0];
-        buffer.push(id);
-      });
-      res.send(buffer);
-    });
-  },
-};
+    await this.#deleteFileList(id);
+    return this.#addFileList(id, title);
+  }
+  async #addFileList(id, title, text) {
+    const fileHandler = await fsPromise.open("files/fileList.txt");
+    const file = await fileHandler.readFile({ encoding: "utf-8" });
+    console.log("file : ", JSON.parse(file));
+    const parsing = JSON.parse(file);
+    const data = [...parsing, { id, text }];
+    const newData = JSON.stringify(data);
+    return fsPromise.writeFile("files/fileList.txt", newData);
+  }
+  async #deleteFileList(id) {
+    const fileHandler = await fsPromise.open("files/fileList.txt");
+    const file = await fileHandler.readFile({ encoding: "utf-8" });
+    const data = JSON.parse(file).filter((post) => post.id !== id);
+    const newData = JSON.stringify(data);
+    return fsPromise.writeFile("files/fileList.txt", newData);
+  }
+  async getFileList() {
+    const fileHandler = await fsPromise.open("files/fileList.txt");
+    return fileHandler.readFile({ encoding: "utf-8" });
+  }
+}
+
+module.exports = File;

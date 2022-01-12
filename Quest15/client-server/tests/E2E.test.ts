13194be5a1c2e6b08fc9f3a65aad9fa4eeb7487d
sangjun.yoon <sangjun.yoon@gmail.com>
2022-01-12 16:34:15 +0900

    refactory : remove flaky test case

--- Quest15/client-server/tests/E2E.test.ts
+++ Quest15/client-server/tests/E2E.test.ts
@@ -1,8 +1,5 @@
 import 'expect-puppeteer';
 
-jest.setTimeout(10000);
-const wait = (time : number) : Promise<void> => new Promise((resolve) => { setTimeout(resolve, time); });
-
 describe('e2e Test', () => {
   beforeAll(async () => {
     await page.goto('https://localhost:3001', { waitUntil: 'domcontentloaded' });
@@ -18,18 +15,24 @@ describe('e2e Test', () => {
     await expect(page).toFill('#js-login-user-name', 'user1');
     await expect(page).toFill('#js-login-password', '1234');
     await expect(page).toClick('button', { text: '로그인' });
-    await wait(100);
+    await page.waitForResponse((req) => req.url() === 'https://localhost/graphql' && req.headers()['content-length'] > '15');
   });
   test('new test', async () => {
     await expect(page).toClick('button', { text: 'new' });
-    await wait(100);
+    await page.waitForResponse(async (req) => {
+      const body = await req?.json?.();
+      return req.url() === 'https://localhost/graphql' && body?.data?.user?.posts?.[0]?.title === 'untitled';
+    });
     const post = await page.$('li.file > .title');
     const postName = await page.evaluate((x) => x.innerText, post);
     expect(postName).toBe('untitled');
   });
   test('save as test', async () => {
     await expect(page).toClick('button', { text: 'save as' });
-    await wait(100);
+    await page.waitForResponse(async (req) => {
+      const body = await req?.json?.();
+      return req.url() === 'https://localhost/graphql' && body?.data?.user?.posts?.[0]?.title === 'test name';
+    });
     const post = await page.$('li.file > .title');
     const postName = await page.evaluate((x) => x.innerText, post);
     expect(postName).toBe('test name');
@@ -37,15 +40,17 @@ describe('e2e Test', () => {
   test('save test', async () => {
     await expect(page).toFill('.text-editor', 'test message');
     await expect(page).toClick('button', { text: 'save' });
-    await wait(100);
     const editor = await page.$('.text-editor');
     const editorText = await page.evaluate((x) => x.innerText, editor);
     expect(editorText).toBe('test message');
   });
   test('deletePost', async () => {
-    const list = await page.$('ul.tab-list');
     await expect(page).toClick('button', { text: 'X' });
-    await wait(100);
+    await page.waitForResponse(async (req) => {
+      const body = await req?.json?.();
+      return req.url() === 'https://localhost/graphql' && body?.data?.user?.posts !== undefined;
+    });
+    const list = await page.$('ul.tab-list');
     const post = await page.evaluate((x) => x.innerText, list);
     expect(post).toBe('');
   });
